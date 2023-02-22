98b3642ef1455cb57e8a2a1004f901816a851a11
Pras <pras.adi.prakasa@gmail.com>
2023-02-22 10:18:17 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/Prasetiaadi/restfull-api-yt/controllers/productcontroller"
+	"github.com/Prasetiaadi/restfull-api-yt/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	models.ConnectDatabase()
+
+	r.GET("/api/products", productcontroller.Index)
+	r.GET("/api/products/:id", productcontroller.Show)
+	r.POST("/api/product", productcontroller.Create)
+	r.PUT("/api/product/:id", productcontroller.Update)
+	r.DELETE("/api/product", productcontroller.Delete)
+
+	r.Run()
+}
